6579fa21ff12621267688f3931a42dd1a728a346
Abhishek-Tom <abhishek210201@gmail.com>
2025-07-17 08:35:17 +0530

    Add new material

--- basicofloops.c
+++ basicofloops.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 int main(){
-    for(int i=1;i<=5;i++){
-        printf("Hello world\n");
+    // for(int i=1;i<=5;i++){
+    //     printf("Hello world\n");
+    // }
+    int n;
+    printf("Enter the number to print Hello world: ");
+    scanf("%d",&n);
+    for(int i=1 ; i<=n; i++){
+        printf("Hello world\n");      
     }
     return 0;
 }
