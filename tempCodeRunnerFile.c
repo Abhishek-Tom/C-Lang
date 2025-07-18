dbd831f987bc1131eb8ca579ca969d10523ac091
Abhishek-Tom <abhishek210201@gmail.com>
2025-07-18 22:57:59 +0530

    Update ethe error

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+    // int n;
+    // printf("Enter the number to print Hello world: ");
+    // scanf("%d",&n);
+    // for(int i=1 ; i<=n; i++){
+    //     printf("Hello world\n");      
+    // }
