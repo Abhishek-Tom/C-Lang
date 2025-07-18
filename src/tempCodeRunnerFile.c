c76e36be5c0a203b241e1c94fb6a082c5cba5883
Abhishek-Tom <abhishek210201@gmail.com>
2025-07-19 01:24:52 +0530

    Ap without maths

--- src/tempCodeRunnerFile.c
+++ src/tempCodeRunnerFile.c
@@ -1,14 +1,1 @@
-#include<stdio.h>
-int main(){
-    int n;   // kahan tk no print krna hai
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-    // for(int i=1; i<=2*n-1; i=i+2){    /// Odd numbers ke liye loop
-    //     printf("%d ",i);
-    // }
-     
-    for(int i=4; i<=n; i=i+3){    /// Odd numbers ke liye loop
-        printf("%d ",i);
-    }
-    return 0;
-}
+int a=a+3;
