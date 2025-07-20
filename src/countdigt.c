27664a0c2e717fb183f27dafc57555582d7add25
Abhishek-Tom <abhishek210201@gmail.com>
2025-07-20 23:37:33 +0530

    Count the digits

--- src/countdigt.c
+++ src/countdigt.c
@@ -3,6 +3,11 @@ int main(){
     int n;
     printf("Enter the number ");
     scanf("%d",&n);
-    
+    int count=0;
+    while(n!=0){
+        n=n/10;
+        count++;
+    }
+    printf("The number of digits are %d",count);
     return 0;
 }
