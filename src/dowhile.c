4ae149d7917783d846d0ece651a7e8be302e9ae5
Abhishek-Tom <abhishek210201@gmail.com>
2025-07-20 23:08:38 +0530

    Do While

---
+++ src/dowhile.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int i=1;
+    // while (i<10)
+    // {
+    //     printf("hello ");
+    //     i++;
+    // }
+
+    do{
+        printf("hello ");
+        i++;
+    } while(i<10);
+    
+    return 0;
+}
