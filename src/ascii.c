a255a9f5b2f0c9ec358253d4e5d1df0655616aef
Abhishek-Tom <abhishek210201@gmail.com>
2025-07-26 03:33:22 +0530

    Ascii values

--- src/ascii.c
+++ src/ascii.c
@@ -5,10 +5,18 @@ int main(){
     // printf("%d\n",x);
     // char ch=(char)x; 
     // printf("%c\n",ch);
-    char ch='A';
+    // char ch='A';
+    // printf("%c\n",ch);
+    // int x=(int)ch;
+    // printf("%d\n",x); 
+
+    for(int i=65;i<=90;i++){ //Ato Z
+    printf("%d ",i);
+    char ch =(char)i;
     printf("%c\n",ch);
-    int x=(int)ch;
-    printf("%d\n",x);
+    }
+
+
 
 
     return 0;
