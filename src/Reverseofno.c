c711834a2bbfc6040f540cef75bbb9b4685a435b
Abhishek-Tom <abhishek210201@gmail.com>
2025-07-26 01:03:28 +0530

    Update Reverse to remove 0

--- src/Reverseofno.c
+++ src/Reverseofno.c
@@ -6,9 +6,9 @@ int main()
     scanf("%d", &n);
     int r= 0;
     while (n>0)
-    {
-        r=r+(n%10);
+    {   
         r=r*10;
+        r=r+(n%10);
         n=n/10;
     }
     printf("The reverse number are %d",r);
