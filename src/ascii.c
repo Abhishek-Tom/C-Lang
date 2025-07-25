2a65652384b05e345d01f638e37756481243f497
Abhishek-Tom <abhishek210201@gmail.com>
2025-07-26 03:26:23 +0530

    Typecasting

---
+++ src/ascii.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    // What is Typecasting
+    // int x=65;
+    // printf("%d\n",x);
+    // char ch=(char)x; 
+    // printf("%c\n",ch);
+    char ch='A';
+    printf("%c\n",ch);
+    int x=(int)ch;
+    printf("%d\n",x);
+
+
+    return 0;
+}
